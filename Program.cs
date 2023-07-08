3d1f39e9b3d90024bfe662700fae72fc26f023d4
Kolbasator2 <kirillchebotarev253@mail.ru>
2023-07-08 10:12:54 +0600

    Tasks was solved

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿//Task 2
+//
+// Console.Write("Input number a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if(a < b)
+// {
+//     max = b;
+// }
+// Console.WriteLine($"Max = {max}");
+//
+//Task 4
+//
+// Console.Write("Input number a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if(a < b)
+// {
+//     max = b;
+// }
+// if(a < c)
+// {
+//     max = c;
+// }
+// Console.WriteLine(max);
+//
+//Task 6
+//
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// bool isEven = number % 2 == 0;
+// if(isEven)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+//
+//Task 8
+//
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int current = 1;
+// while(current <= number)
+// {
+//     if(current % 2 == 0)
+//     {
+//         Console.WriteLine(current);
+//     }
+//     current++;
+// }
