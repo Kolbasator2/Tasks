4567d618a0a857508f7d56f55a9b5794d2d6c4a4
Kolbasator2 <kirillchebotarev253@mail.ru>
2023-07-15 09:46:59 +0600

    Tasks 10, 13, 15 were solved

--- Program.cs
+++ Program.cs
@@ -57,3 +57,70 @@
 //     }
 //     current++;
 // }
+//
+// Task 10
+//
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int thirdChar = number % 10;
+// int secondChar = ((number % 100) - thirdChar) / 10;
+// Console.WriteLine(secondChar);
+//
+// Task 13
+//
+// Console.Write("Input number: ");
+// string number = Console.ReadLine();
+// if(number.Length <= 2)
+// {
+//     Console.WriteLine("The number is too small");
+// }
+// else
+// {
+//     Console.WriteLine(number[2]);
+// }
+//
+// Task 13 
+//
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number < 100)
+// {
+//     Console.WriteLine("The number is too small");
+// }
+// else
+// {
+//     int lengthOfNumber = 0;
+//     int currentDigit = 1;
+//     while(Math.Pow(10, lengthOfNumber) < number)
+//     {
+//         lengthOfNumber++;
+//         currentDigit++;
+//     }
+//     for(int i = 0; i < lengthOfNumber; i++)
+//     {
+//         if(number / Math.Pow(10, i) < 1000)
+//         {
+//             number = Convert.ToInt32(Math.Truncate(number / Math.Pow(10, i)));
+//             int result = number % 10;
+//             Console.WriteLine(result);
+//             break;
+//         }
+//     }
+// }
+//
+// Task 15
+//
+// Console.Write("Input № of day: ");
+// int numberOfDay = Convert.ToInt32(Console.ReadLine());
+// if(numberOfDay < 0)
+// {
+//     Console.WriteLine("Incorrect input");
+// }
+// else if(numberOfDay <= 5)
+// {
+//     Console.WriteLine("No");
+// }
+// else
+// {
+//     Console.WriteLine("Yes");
+// }
