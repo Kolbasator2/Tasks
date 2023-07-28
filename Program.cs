ea483467bb71ef32f06415eaa5c0195c84bd3695
Kolbasator2 <kirillchebotarev253@mail.ru>
2023-07-28 16:49:07 +0600

    Tasks 34, 36 and 38 were solved

--- Program.cs
+++ Program.cs
@@ -191,3 +191,99 @@
 //     numbers[i] = new Random().Next(-100, 99);
 // }
 // Console.WriteLine($"[{string.Join(", ", numbers)}]");
+//
+// Task 34
+//
+// int[] generateArray(int length)
+// {
+//     int[] result = new int[length];
+//     for(int i = 0; i < length; i++)
+//     {
+//         result[i] = new Random().Next(101, 999);
+//     }
+//     return result;
+// }
+// int findNumberOfEvenElements(int[] array)
+// {
+//     int result = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] % 2 == 0)
+//         {
+//             result++;
+//         }
+//     }
+//     return result;
+// }
+// Console.Write("Input length of array: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// int[] array = generateArray(length);
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// Console.WriteLine($"Number of even elements equals to {findNumberOfEvenElements(array)}");
+//
+// Task 36 
+//
+// int[] generateArray(int length)
+// {
+//     int[] result = new int[length];
+//     for(int i = 0; i < length; i++)
+//     {
+//         result[i] = new Random().Next(-100, 99);
+//     }
+//     return result;
+// }
+// int findSumOfElementsWithOddIndexes(int[] array)
+// {
+//     int result = 0;
+//     for(int i = 1; i < array.Length; i+=2)
+//     {
+//         result += array[i];
+//     }
+//     return result;
+// }
+// Console.Write("Input length of array: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// int[] array = generateArray(length);
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// Console.WriteLine($"Sum of elements with odd indexes equals to {findSumOfElementsWithOddIndexes(array)}");
+//
+// Task 38
+//
+// double[] generateArray(int length)
+// {
+//     double[] result = new double[length];
+//     for(int i = 0; i < length; i++)
+//     {
+//         result[i] = new Random().Next(-100, 99) * Math.PI;
+//     }
+//     return result;
+// }
+// double getMaxElement(double[] array)
+// {
+//     double max = array[0];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(max <= array[i])
+//         {
+//             max = array[i];
+//         }
+//     }
+//     return max;
+// }
+// double getMinElement(double[] array)
+// {
+//     double min = array[0];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(min >= array[i])
+//         {
+//             min = array[i];
+//         }
+//     }
+//     return min;
+// }
+// Console.Write("Input length of array: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// double[] array = generateArray(length);
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// Console.WriteLine($"Difference between max and min elements equals to {getMaxElement(array) - getMinElement(array)}");
