dc8f23c823063c35b4d7e2c0ec7fc80db72181dc
Kolbasator2 <kirillchebotarev253@mail.ru>
2023-08-05 09:05:22 +0600

    Tasks 64, 66 and 68 were solved

--- Program.cs
+++ Program.cs
@@ -701,3 +701,61 @@
 //     Console.WriteLine("\n\n");
 // }
 // showMatrix(generateMatrix());
+//
+// Task 64 
+// 
+// void printNumbers(int N)
+// {
+//     if(N < 1)
+//     {
+//         return;
+//     }
+//     Console.Write($"{N} ");
+//     N--;
+//     printNumbers(N);
+// }
+// Console.Write("Input N: ");
+// printNumbers(Convert.ToInt32(Console.ReadLine()));
+//
+// Task 66
+//
+// int findSum(int M, int N)
+// {
+//     if(N == M)
+//     {
+//         return N;
+//     }
+//     return N + findSum(M, N - 1);
+// }
+// Console.Write("Input N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Sum of natural numbers from M to N equals to {findSum(M, N)}");
+//
+// Task 68
+// 
+// int A(int m, int n)
+// {
+//     if(m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if(m > 0 && n == 0)
+//     {
+//         return A(m - 1, 1);
+//     }
+//     else if(m > 0 && n > 0)
+//     {
+//         return A(m - 1, A(m, n - 1));
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+// Console.Write("Input m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(A(m, n));
